53846bb19f986d54b0ecae4cb4f53728a428821f
rakkyo150 <86054813+rakkyo150@users.noreply.github.com>
2022-10-16 01:13:21 +0900

    エラー処理追加

--- main.py
+++ main.py
@@ -15,134 +15,137 @@ import db_handler
 from dotenv import load_dotenv
 load_dotenv()
 
-link="https://osu.ppy.sh/beatmaps/packs"
+try:
+    link="https://osu.ppy.sh/beatmaps/packs"
 
-options=Options()
-options.add_argument("--headless")
+    options=Options()
+    options.add_argument("--headless")
 
-# driver=webdriver.Chrome(executable_path=os.environ["CHROME_DRIVER"],options=options)
-driver=webdriver.Chrome(options=options)
+    # driver=webdriver.Chrome(executable_path=os.environ["CHROME_DRIVER"],options=options)
+    driver=webdriver.Chrome(options=options)
 
-# 暗黙的待機
-driver.implicitly_wait(10)
-wait=WebDriverWait(driver,30)
+    # 暗黙的待機
+    driver.implicitly_wait(10)
+    wait=WebDriverWait(driver,30)
 
-driver.get(link)
-wait.until(EC.presence_of_all_elements_located)
+    driver.get(link)
+    wait.until(EC.presence_of_all_elements_located)
 
-collapse=driver.find_element_by_class_name("fa-chevron-down")
-collapse.click()
-wait.until(EC.presence_of_all_elements_located)
+    collapse=driver.find_element_by_class_name("fa-chevron-down")
+    collapse.click()
+    wait.until(EC.presence_of_all_elements_located)
 
-loginPopup=driver.find_element_by_class_name("avatar")
-loginPopup.click()
-wait.until(EC.presence_of_all_elements_located)
+    loginPopup=driver.find_element_by_class_name("avatar")
+    loginPopup.click()
+    wait.until(EC.presence_of_all_elements_located)
 
-username=driver.find_element_by_name("username")
-username.send_keys(os.environ["USER_NAME"])
+    username=driver.find_element_by_name("username")
+    username.send_keys(os.environ["USER_NAME"])
 
-password=driver.find_element_by_name("password")
-password.send_keys(os.environ["PASSWORD"])
+    password=driver.find_element_by_name("password")
+    password.send_keys(os.environ["PASSWORD"])
 
-loginButton=driver.find_element_by_class_name("fa-sign-in-alt")
-loginButton.click()
-wait.until(EC.presence_of_all_elements_located)
-# ブラウザのログイン処理待ち
-sleep(3)
+    loginButton=driver.find_element_by_class_name("fa-sign-in-alt")
+    loginButton.click()
+    wait.until(EC.presence_of_all_elements_located)
+    # ブラウザのログイン処理待ち
+    sleep(3)
 
-response=driver.page_source.encode("utf-8")
-soup=BeautifulSoup(response,"html.parser")
-packList=soup.find("div",class_="js-accordion")
-topPackDiv=packList.div
+    response=driver.page_source.encode("utf-8")
+    soup=BeautifulSoup(response,"html.parser")
+    packList=soup.find("div",class_="js-accordion")
+    topPackDiv=packList.div
 
-topDataPackId=topPackDiv.get("data-pack-id")
-topPackName=topPackDiv.a.div.text
+    topDataPackId=topPackDiv.get("data-pack-id")
+    topPackName=topPackDiv.a.div.text
 
-avoidWord=["taiko","catch","mania"]
+    avoidWord=["taiko","catch","mania"]
 
-oldDataPackId=db_handler.exportOldBeatmapPack()
-print(oldDataPackId)
+    oldDataPackId=db_handler.exportOldBeatmapPack()
+    print(oldDataPackId)
 
-# 初回
-if oldDataPackId is None:
-    packDivs = packList.find_all("div", class_="beatmap-pack")
-    for pack in packDivs:
-        packName = pack.a.div.text
+    # 初回
+    if oldDataPackId is None:
+        packDivs = packList.find_all("div", class_="beatmap-pack")
+        for pack in packDivs:
+            packName = pack.a.div.text
 
-        if any(words in packName.lower() for words in avoidWord):
-            pass
-        else:
-            packContentLink=pack.a.get("href")
-            driver.get(packContentLink)
-            wait.until(EC.presence_of_element_located((By.CLASS_NAME,"beatmap-pack-download__link")))
-            packContentHtml=driver.page_source.encode("utf-8")
-            packContentSoup=BeautifulSoup(packContentHtml,"html.parser")
-            packDownloadA=packContentSoup.find("a",class_="beatmap-pack-download__link")
-
-            content="**初実行！**\nhttps://osu.ppy.sh/beatmaps/packs"
-            description="**"+packName+"**\n"+packDownloadA.get("href")
-
-            webhookUrl = os.environ["WEBHOOK_URL"]
-            payload = {
-                "content": content,
-                "embeds": [{
-                    "description": description,
-                    "color":15753632
-                }],
-            }
-            headers = {"Content-Type": "application/json"}
-
-            requests.post(webhookUrl, json.dumps(payload), headers=headers)
-
-            break
-    db_handler.updateNewBeatmapPack("firstExecution", topDataPackId)
-
-# 更新してるかどうか
-elif oldDataPackId==topDataPackId:
-    pass
-else:
-    description=""
-    newPack=False
-    packDivs=packList.find_all("div",class_="beatmap-pack")
-    for pack in packDivs:
-        # 直近の更新までループが終わったとき
-        if str(pack.get("data-pack-id"))==oldDataPackId:
-            # 更新あったとき
-            if newPack is True:
-                db_handler.updateNewBeatmapPack(oldDataPackId, topDataPackId)
-            break
-        else:
-            newPack=True
-            # 更新内容をosu!stdとそれ以外で分けてnewPacksNameを作る
-            packName=pack.a.div.text
             if any(words in packName.lower() for words in avoidWord):
                 pass
             else:
-                packContentLink = pack.a.get("href")
+                packContentLink=pack.a.get("href")
                 driver.get(packContentLink)
                 wait.until(EC.presence_of_element_located((By.CLASS_NAME,"beatmap-pack-download__link")))
-                packContentHtml = driver.page_source.encode("utf-8")
-                packContentSoup = BeautifulSoup(packContentHtml, "html.parser")
-                packDownloadA = packContentSoup.find("a",class_="beatmap-pack-download__link")
+                packContentHtml=driver.page_source.encode("utf-8")
+                packContentSoup=BeautifulSoup(packContentHtml,"html.parser")
+                packDownloadA=packContentSoup.find("a",class_="beatmap-pack-download__link")
 
-                description+="**"+packName+"**\n"+packDownloadA.get("href")+"\n"
+                content="**初実行！**\nhttps://osu.ppy.sh/beatmaps/packs"
+                description="**"+packName+"**\n"+packDownloadA.get("href")
 
-    # 更新内容がosu!std以外しかないorそもそも更新していない場合はディスコートに通知しない
-    if description=="":
+                webhookUrl = os.environ["WEBHOOK_URL"]
+                payload = {
+                    "content": content,
+                    "embeds": [{
+                        "description": description,
+                        "color":15753632
+                    }],
+                }
+                headers = {"Content-Type": "application/json"}
+
+                requests.post(webhookUrl, json.dumps(payload), headers=headers)
+
+                break
+        db_handler.updateNewBeatmapPack("firstExecution", topDataPackId)
+
+    # 更新してるかどうか
+    elif oldDataPackId==topDataPackId:
         pass
     else:
-        content=f"**ビートマップパック更新！**\nhttps://osu.ppy.sh/beatmaps/packs\n"
+        description=""
+        newPack=False
+        packDivs=packList.find_all("div",class_="beatmap-pack")
+        for pack in packDivs:
+            # 直近の更新までループが終わったとき
+            if str(pack.get("data-pack-id"))==oldDataPackId:
+                # 更新あったとき
+                if newPack is True:
+                    db_handler.updateNewBeatmapPack(oldDataPackId, topDataPackId)
+                break
+            else:
+                newPack=True
+                # 更新内容をosu!stdとそれ以外で分けてnewPacksNameを作る
+                packName=pack.a.div.text
+                if any(words in packName.lower() for words in avoidWord):
+                    pass
+                else:
+                    packContentLink = pack.a.get("href")
+                    driver.get(packContentLink)
+                    wait.until(EC.presence_of_element_located((By.CLASS_NAME,"beatmap-pack-download__link")))
+                    packContentHtml = driver.page_source.encode("utf-8")
+                    packContentSoup = BeautifulSoup(packContentHtml, "html.parser")
+                    packDownloadA = packContentSoup.find("a",class_="beatmap-pack-download__link")
 
-        webhookUrl = os.environ["WEBHOOK_URL"]
-        payload={
-            "content":content,
-            "embeds":[{
-                "description":description,
-                "color": 15753632
-            }],
-        }
-        headers={"Content-Type": "application/json"}
+                    description+="**"+packName+"**\n"+packDownloadA.get("href")+"\n"
 
-        requests.post(webhookUrl,json.dumps(payload),headers=headers)
+        # 更新内容がosu!std以外しかないorそもそも更新していない場合はディスコートに通知しない
+        if description=="":
+            pass
+        else:
+            content=f"**ビートマップパック更新！**\nhttps://osu.ppy.sh/beatmaps/packs\n"
 
-driver.close()
+            webhookUrl = os.environ["WEBHOOK_URL"]
+            payload={
+                "content":content,
+                "embeds":[{
+                    "description":description,
+                    "color": 15753632
+                }],
+            }
+            headers={"Content-Type": "application/json"}
+
+            requests.post(webhookUrl,json.dumps(payload),headers=headers)
+
+    driver.close()
+except Exception as e:
+    print(e)
